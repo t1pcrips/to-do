382c0c15c816bc988f89c3e859441b11ae70c703
Timofey Podlinov <timapodina@gmail.com>
2024-10-31 18:10:07 +0300

    created POST method for tasks and upgraded GET method

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
@@ -9,15 +10,41 @@ import (
 
 var (
 	port = "8080"
+	task string
+	rb   requestBody
 )
 
+type requestBody struct {
+	Message string `json:"message"`
+}
+
+type WriterMessage struct {
+	Message string
+}
+
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World!")
+	log.Println("Hello task")
+
+	fmt.Fprintf(w, "Hello, %s", task)
+}
+
+func CreateNewTask(w http.ResponseWriter, r *http.Request) {
+	log.Println("New task create")
+
+	json.NewDecoder(r.Body).Decode(&rb)
+	task = rb.Message
+
+	w.WriteHeader(200)
+	msg := WriterMessage{Message: "new task created"}
+	fmt.Fprint(w, msg.Message)
 }
 
 func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/task", CreateNewTask).Methods("POST")
+
+	log.Println("Server starts")
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
